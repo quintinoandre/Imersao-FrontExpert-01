01357a5ac66984e36ed8c6f44639d6d110647ae7
pandrequintino@gmail.com <pandrequintino@gmail.com>
2022-04-12 23:26:03 +0100

    feat: atualizado footer

--- front-on-food/components/footer/footer.tsx
+++ front-on-food/components/footer/footer.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Styles from './footer.module.css';
+
 function Footer() {
 	return (
 		<footer>
-			<p>Imersão FrontExpert</p>
+			<div className={Styles.footer}>
+				<p>@ 2022 - Imersão FrontExpert</p>
+			</div>
 		</footer>
 	);
 }
