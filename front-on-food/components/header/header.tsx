56ae963ec40a5dc47e8a2fa877c4b1a44e617839
pandrequintino@gmail.com <pandrequintino@gmail.com>
2022-04-12 23:32:18 +0100

    feat: atualizado header

--- front-on-food/components/header/header.tsx
+++ front-on-food/components/header/header.tsx
@@ -1,20 +1,43 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+
+import { IconBookmark, IconHome } from '@icons';
+
+import Styles from './header.module.css';
 
 function Header() {
 	return (
 		<header>
-			<nav className="max-w-7xl mx-auto py-8 flex items-center justify-between">
-				<div className="bg-gray-200">[Logo]</div>
-				<ul className="flex">
+			<nav className={Styles.nav}>
+				<div className={Styles.brand}>
+					<Link href="/">
+						<a>
+							<Image
+								src="/images/logo.png"
+								width={45}
+								height={36}
+								alt="OnFood"
+							/>
+							<span>OnFood</span>
+						</a>
+					</Link>
+				</div>
+				<ul>
 					<li>
 						<Link href="/">
-							<a>Home</a>
+							<a>
+								<IconHome />
+								Home
+							</a>
 						</Link>
 					</li>
 					<li>
 						<Link href="/favorites">
-							<a>Favorites</a>
+							<a>
+								<IconBookmark />
+								Favorites
+							</a>
 						</Link>
 					</li>
 				</ul>
